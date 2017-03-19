e75b51c3d2320de7e42707dc739291ee49c59c8c
ndxbxrme <lewis_the_cat@hotmail.com>
2017-03-19 13:17:12 +0000

    linux user

--- init-all.sh
+++ init-all.sh
@@ -4,6 +4,8 @@ npm install
 npm install -g grunt-cli
 bower install
 grunt build
+groupadd -r vsagency && useradd -m -r -g vsagency vsagency
+su vsagency
 . env.sh
 screen -X -S VSAGENCY quit
 screen -S VSAGENCY node --expose-gc server/app.js
