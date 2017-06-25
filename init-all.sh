2f12974acd971e8c6a0991802b5d42e1f0292b0d
ndxbxrme <lewis_the_cat@hotmail.com>
2017-06-25 21:43:41 +0100

    init-all detatch screen

--- init-all.sh
+++ init-all.sh
@@ -7,4 +7,4 @@ grunt build
 #groupadd -r vsagency && useradd -m -r -g vsagency vsagency
 su vsagency -c ". env.sh
 screen -X -S VSAGENCY quit || true
-screen -S VSAGENCY node --expose-gc server/app.js"
+screen -d -m -S VSAGENCY node --expose-gc server/app.js"
