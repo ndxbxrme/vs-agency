b17ec53af6e0fd96f8960aadb797ef7ce32b87fc
root <root@mmd-web-01.manchestermade.com>
2020-07-05 21:16:13 +0100

    new server

--- init-all.sh
+++ init-all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+rm -rf package-lock.json
 git pull
 npm install
 npm install -g grunt-cli
 bower install
 grunt build
-#groupadd -r vsagency && useradd -m -r -g vsagency vsagency
+# groupadd -r vsagency && useradd -m -r -g vsagency vsagency
 su vsagency -c ". env.sh
 screen -X -S VSAGENCY quit || true
 screen -S VSAGENCY node --expose-gc server/app.js"
