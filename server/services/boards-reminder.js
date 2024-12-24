91051e2c72b996aeaa76040ca78bd77decfcbd4d
ndxbxrme <lewis_the_cat@hotmail.com>
2024-12-24 15:05:20 +0000

    boards reminder email

--- server/services/boards-reminder.js
+++ server/services/boards-reminder.js
@@ -1,47 +1,45 @@
 const { zonedTimeToUtc, utcToZonedTime, format } = require('date-fns-tz');
 
-const millisecondsUntil10am = () => {
+const millisecondsUntilNextWednesdayAt10am = () => {
   const now = new Date();
   const timeZone = 'Europe/London'; // Time zone for the United Kingdom
 
   // Convert the current time to the UK time zone
   const nowInUKTimeZone = utcToZonedTime(now, timeZone);
 
-  const tenAM = new Date(nowInUKTimeZone);
-  tenAM.setHours(10, 0, 0, 0);
+  const nextWednesday = new Date(nowInUKTimeZone);
+  nextWednesday.setHours(10, 0, 0, 0);
 
-  if (nowInUKTimeZone.getHours() >= 10 || nowInUKTimeZone.getTime() > tenAM.getTime()) {
-    tenAM.setDate(tenAM.getDate() + 1);
-  }
+  // Determine the day difference to next Wednesday
+  const daysUntilWednesday = (10 - nowInUKTimeZone.getDay()) % 7 || 7;
+  nextWednesday.setDate(nextWednesday.getDate() + daysUntilWednesday);
 
-  // Format the dates for display
-  const formatOptions = { timeZone, hour: '2-digit', minute: '2-digit' };
-  const tenAMString = format(tenAM, 'HH:mm', formatOptions);
-  const nowString = format(nowInUKTimeZone, 'HH:mm', formatOptions);
-
-  console.log(`The next email will be sent at 10 AM UK time. Current time: ${nowString}, Next email time: ${tenAMString}`);
-
-  return tenAM.getTime() - nowInUKTimeZone.getTime();
+  return nextWednesday.getTime() - nowInUKTimeZone.getTime();
 };
 module.exports = (ndx) => {
   ndx.database.on('ready', () => {
     const sendEmail = async () => {
-      if(new Date().getDay()===3) {
-        const template = await ndx.database.selectOne('emailtemplates', {
-          name: 'Auto Reminder - Boards'
-        });
-        if(template) {
-          const users = await ndx.database.select('users');
-          if(users && users.length) {
-            for(user of users) {
-              template.to = user.local.email;
-              ndx.email.send(template);
+      try {
+        if(new Date().getDay()===3) {
+          const template = await ndx.database.selectOne('emailtemplates', {
+            name: 'Auto Reminder - Boards'
+          });
+          if(template) {
+            const users = await ndx.database.select('users');
+            if(users && users.length) {
+              for(const user of users) {
+                template.to = user.local.email;
+                ndx.email.send(template);
+              }
             }
           }
         }
       }
-      setTimeout(sendEmail, millisecondsUntil10am());
+      catch(e) {
+        console.log('boards reminder error');
+      }
+      setTimeout(sendEmail, millisecondsUntilNextWednesdayAt10am());
     }
-    setTimeout(sendEmail, millisecondsUntil10am());
+    setTimeout(sendEmail, millisecondsUntilNextWednesdayAt10am());
   })
 }
